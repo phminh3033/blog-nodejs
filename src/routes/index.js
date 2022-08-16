45b7b9bb9183b75bef0f407cb14484484b1eafd5
phminh3033 <phminh3033@gmail.com>
2022-08-16 09:00:53 +0700

    i prettier, lint-staged, husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
-const newsRouter = require('./news')
-const siteRouter = require('./site')
+const newsRouter = require('./news');
+const siteRouter = require('./site');
 
-function route (app) {
-    // app.get('/', (req, res) => {
-    //     res.render('home');
-    // });
-      
-    app.use('/news', newsRouter)
-    app.use('/', siteRouter)
+function route(app) {
+  // app.get('/', (req, res) => {
+  //     res.render('home');
+  // });
+
+  app.use('/news', newsRouter);
+  app.use('/', siteRouter);
 }
 
-module.exports = route
+module.exports = route;
