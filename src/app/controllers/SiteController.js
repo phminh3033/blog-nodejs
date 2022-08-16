c701a776be399ee54846d3c81d123fb1c7476425
phminh3033 <phminh3033@gmail.com>
2022-08-16 10:25:31 +0700

    [MVC] Model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,16 @@
+const Course = require('../models/Course')
+
 class SiteController {
     //[GET] /
     index(req, res) {
-        res.render('home');
+        Course.find({}, function(err, cources){
+            if (!err) {
+                res.json(cources)
+            } else {
+                res.status(400).json({error: 'Error'})
+            }
+        })
+        // res.render('home');
     }
 
     //[GET] /search
---
+++ src/config/db/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+async function connect () {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/f8_edu_dev');
+        console.log('success');
+    } catch (error) {
+        console.log('failed to connect');
+        
+    }
+}
+
+module.exports = {connect};
