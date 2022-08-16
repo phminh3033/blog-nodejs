80ff534075c4555edf1f5bef2907f3a9b914a4bc
phminh3033 <phminh3033@gmail.com>
2022-08-16 15:35:16 +0700

    [CRUD] Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,26 @@
 const Course = require('../models/Course')
+const { mutipleMongooseToObject } = require('../../utils/mongoose')
 
 class SiteController {
     //[GET] /
-    index(req, res) {
-        Course.find({}, function(err, cources){
-            if (!err) {
-                res.json(cources)
-            } else {
-                res.status(400).json({error: 'Error'})
-            }
-        })
+    index(req, res, next) {
+        //promise
+        Course.find({})
+            .then(courses => {
+                res.render('home', { 
+                    courses: mutipleMongooseToObject(courses)
+                })
+            })
+            .catch(next)
+
+        //func
+        // Course.find({}, function(err, cources){
+        //     if (!err) {
+        //         res.json(cources)
+        //     } else {
+        //         res.status(400).json({error: 'Error'})
+        //     }
+        // })
         // res.render('home');
     }
 
