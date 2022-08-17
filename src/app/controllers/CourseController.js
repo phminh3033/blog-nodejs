932582eb49e6f5054a6f01545853c3f0966afce4
phminh3033 <phminh3033@gmail.com>
2022-08-17 10:42:38 +0700

    [CRUD] Delete course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -37,9 +37,16 @@ class CourseController {
 
     //[PUT] /courses/:id
     update(req, res, next) {
-        Course.updateOne({ _id: req.params.id}, req.body)
+        Course.updateOne({ _id: req.params.id }, req.body)
         .then(() => res.redirect('/me/stored/courses'))
         .catch(next)
+    }
+
+    //[DELETE] /courses/:id
+    destroy(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next)
     }
 }
 
